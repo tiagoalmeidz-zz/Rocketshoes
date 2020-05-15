77d29ca57ae120e0e63e3fe5823ef50a8d22f938
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-05-14 23:06:55 -0300

    Estoque na adicao

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -10,12 +10,22 @@ function* addToCart({ id }) {
     state.cart.find(product => product.id === id)
   );
 
-  if (productExists) {
-    const amount = productExists.amount + 1;
+  const stock = yield call(api.get, `/stock/${id}`);
 
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERRO! O estoque desse produto acabou.');
+    return;
+  }
+
+  if (productExists) {
     yield put(updateAmount(id, amount));
   } else {
-    const response = yield call(api.get, `products/${id}`);
+    const response = yield call(api.get, `/products/${id}`);
 
     const data = {
       ...response.data,
