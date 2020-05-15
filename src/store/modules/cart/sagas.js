355fbe7d361b065487f5546e235ee7a0609cbb17
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-05-14 22:21:02 -0300

    Saga + Reactotron

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,10 +1,11 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, all, takeLatest, delay } from 'redux-saga/effects';
 import api from '../../../services/api';
 
 import { addToCartSuccess } from './actions';
 import ActionsCart from './const';
 
 function* addToCart({ id }) {
+  yield delay(2000);
   const response = yield call(api.get, `products/${id}`);
 
   yield put(addToCartSuccess(response.data));
