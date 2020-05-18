5810444aa8300c424a204505610b0bab1b6b10e9
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-05-18 00:06:01 -0300

    Tratamento da API com variavel de ambiente em dev e prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/tiagoalmeida93/api-rocketshoes',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'https://my-json-server.typicode.com/tiagoalmeida93/api-rocketshoes'
+      : 'http://localhost:3333',
 });
 
 export default api;
